bb325bf61299d5cc9fe36120916f701aaaba2160
clgns rla <chihungim@gmail.com>
2022-11-15 11:10:17 +0900

    solving...

---
+++ D2_Theatre Square/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <cassert>
+#include <set>
+
+using std::cin;
+using std::cout;
+
+int main(int argc, char* argv[]) {
+	double n, m, a;
+	cin >> n >> m >> a;
+	cout << ceil(n / a) + ceil(m / a);
+	return 0;
+}
