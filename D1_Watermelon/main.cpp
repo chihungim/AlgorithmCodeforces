b0437465ec3186493a8a30d09b787534895b8b6a
clgns rla <chihungim@gmail.com>
2022-11-15 10:39:38 +0900

    added solutions

--- D1_Watermelon/main.cpp
+++ D1_Watermelon/main.cpp
@@ -13,6 +13,11 @@
 using std::cin;
 using std::cout;
 
-int main(int argc, char* argv[]) {
+// w를 입력 받은 후 짝수로 나뉠 수 있는지 확인 하는 문제 
 
+int main(int argc, char* argv[]) {
+    int w = 0;
+    cin >> w;
+    cout << (w % 2 == 0 && w > 2 ? "YES" : "NO");
+    return 0;
 }
